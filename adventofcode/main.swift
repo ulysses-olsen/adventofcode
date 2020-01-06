d73d7d5e70e3d101d0a89d4fe55d1be18466a9af
Todd Olsen <todd.olsen@me.com>
2020-01-06 07:33:04 -0500

    Initial Commit

---
+++ adventofcode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  adventofcode
+//
+//  Created by Todd Olsen on 1/6/20.
+//  Copyright © 2020 proxpero. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
